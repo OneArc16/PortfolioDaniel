41010d1eb6312248ac7b271967dbcb7a27128569
Daniel <danielcast244@gmail.com>
2023-07-16 14:11:16 -0500

    SE HACE PAGINA PRINCIAPL INCIANDO CON E, SIDEBAR

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,5 +2,12 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className="grid grid-cols-12 gap-6 px-48 lg:my-14 px:-5"> 
+      <div className="col-span-12 bg-white lg:col-span-3 rounded-2x1">Sidebar</div>
+        <div className="col-span-12 bg-white lg:col-span-9 rounded-2x1">
+          <Component {...pageProps} />
+        </div>
+    </div>
+  )
 }
