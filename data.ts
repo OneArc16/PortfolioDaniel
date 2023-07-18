71f696dd12624d337d03290c56c4d7ad9fec4dbf
Daniel <danielcast244@gmail.com>
2023-07-18 14:05:32 -0500

    SE CREA INTERFACES Y ARCHIVO DATA.TS

---
+++ data.ts
@@ -0,0 +1,15 @@
+import {RiComputerLine} from 'react-icons/ri'
+import { IService } from './type'
+
+export const services:IService[] = [
+    {
+        title: "Frontend Dev",
+        about: "lore...",
+        Icon: RiComputerLine,
+    },
+    {
+        title: "title",
+        about: "lore...",
+        Icon: RiComputerLine,
+    },
+]
