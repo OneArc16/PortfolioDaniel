781ca21c904277d4d7bffd2c4c2a8ea95dcea01d
Daniel <danielcast244@gmail.com>
2023-07-19 08:00:16 -0500

    SE DIO DISEÑO CSS EN SERVICECARD, APP E INDEX

--- components/ServiceCard.tsx
+++ components/ServiceCard.tsx
@@ -4,12 +4,17 @@ import { FunctionComponent } from "react"
 const ServiceCard:FunctionComponent<{service:IService}> = ({
     service:{Icon,about,title},
 }) => {
+
+    const createMarkup = () => {
+        return {__html:about}
+    }
+
   return (
-    <div>
-        <Icon />
+    <div className="flex items-center p-2 space-x-4">
+        <Icon className="w-12 h-12 text-green" />
         <div>
-            <h4>{title}</h4>
-            <p>{about}</p>
+            <h4 className="font-bold">{title}</h4>
+            <p dangerouslySetInnerHTML={createMarkup()} />
         </div>
     </div>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,18 +5,22 @@ import { GetServerSidePropsContext, GetStaticPropsContext } from "next"
 const index = () => {
   
   return (
-    <div>
-      <h5>Actualmente estoy cursando la carrera universitaria Ingenieria en Sistemas
+    <div className="flex flex-col flex-grow px-6 pt-1">
+      <h5 className="my-3 font-medium">
+        Actualmente estoy cursando la carrera universitaria Ingenieria en Sistemas
         en la Universidad Nacional Abierta y a Distancia (UNAD). Soy FrontEnd Devoloper Junior 
         con aspiraciones de ser Full Stack Develoṕer.  
       </h5>
 
-      <div>
-        <h6>What I Offer</h6>
-        <div>
+      <div className="flex-grow p-4 mt-5 bg-gray-400" 
+        style={{marginLeft:'-1.5rem', marginRight:'-1.5rem'}}>
+        <h6 className="my-3 text-xl font-bold tracking-wide">What I Offer</h6>
+        <div className="grid gap-6 lg:grid-cols-2">
           {
             services.map(service => (
-            <ServiceCard service={service}/>
+              <div className="bg-gray-200 rounded-lg lg:col-span-1">
+                <ServiceCard service={service}/>
+              </div>
             ))
           }
         </div>
